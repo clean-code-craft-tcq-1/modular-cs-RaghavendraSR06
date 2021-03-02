c02e990163ad11220d74353761601c4b5ad7992b
RaghavendraSR06 <raghu99459@gmail.com>
2021-03-02 20:48:58 +0530

    Update ColorPair.cs

--- TelCo.ColorCoder/ColorPair.cs
+++ TelCo.ColorCoder/ColorPair.cs
@@ -3,41 +3,25 @@ using System.Drawing;
 
 namespace TelCo.ColorCoder
 {
-    public partial class ColorPair
+    public class ColorPair
     {
 
-        public static Color[] MapMajor, MapMinor;
-        internal Color Major, Minor;
+        public static Color[] MapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+        public static Color[] MapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
 
+        public Color Major;
+        public Color Minor;
+
+        public ColorPair(Color majorColor, Color minorColor)
+        {
+            Major = majorColor;
+            Minor = minorColor;
+        }
+        
         public override string ToString()
         {
             return string.Format("MajorColor:{0}, MinorColor:{1}", Major.Name, Minor.Name);
         }
 
-        public static ColorPair GetColorFromPairNumber(int pairNumber)
-        {
-            // The function supports only 1 based index. Pair numbers valid are from 1 to 25
-            int minorSize = MapMinor.Length;
-            int majorSize = MapMajor.Length;
-
-            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
-            {
-                throw new ArgumentOutOfRangeException(
-                    string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
-            }
-
-            // Find index of major and minor color from pair number
-            int zeroBasedPairNumber = pairNumber - 1;
-            int majorIndex = zeroBasedPairNumber / minorSize;
-            int minorIndex = zeroBasedPairNumber % minorSize;
-
-            ColorPair pair = new ColorPair()
-            {
-                Major = MapMajor[majorIndex],
-                Minor = MapMinor[minorIndex]
-            };
-
-            return pair;
-        }
     }
 }
